b075a066fa6fdf3ca2fb670f1405338fba9444d2
Rony Joy <ronyjoy80@gmail.com>
2023-05-05 20:07:59 +0530

    Square Sprite Class Completed

--- square.py
+++ square.py
@@ -2,8 +2,10 @@ import pygame
 
 
 class Square(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x, y):
         super().__init__()
         self.image = pygame.image.load("Resource/square.jpg")
         self.rect = self.image.get_rect()
         self.color = (255, 255, 255)
+        self.grid_pos_x = x
+        self.grid_pos_y = y
